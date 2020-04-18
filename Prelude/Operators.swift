ee7a68e02da80c8742ee8027f70fa831a1826c86
Ryan Stone <ryan@ryanstone.io>
2020-04-18 10:54:20 -0400

    Update operators

--- Prelude/Operators.swift
+++ Prelude/Operators.swift
@@ -69,3 +69,46 @@ infix operator <<< : FunctionCompositionPrecedence
 
 /// Cons of an element with a non-empty collection.
 infix operator +|: AdditionPrecedence
+
+//infix operator >>- : FunctionCompositionPrecedence
+//
+//public func >>- <T, U>(a: T?, f: (T) -> U?) -> U? {
+//  return a.flatMap(f)
+//}
+
+public func flip<A, B, C>(_ f: @escaping (A) -> (B) -> C) -> (B) -> (A) -> C {
+  return { b in
+    { a in
+      f(a)(b)
+    }
+  }
+}
+infix operator >-> : FunctionCompositionPrecedence
+/**
+  compose two functions that produce optional values, from left to right
+  - If the result of the first function is `.none`, the second function will
+    not be inoked and this will return `.none`
+  - If the result of the first function is `.some`, the value is unwrapped and
+    passed to the second function which may return `.none`
+  - parameter f: A transformation function from type `T` to type `Optional<U>`
+  - parameter g: A transformation function from type `U` to type `Optional<V>`
+  - returns: A function from type `T` to type `Optional<V>`
+*/
+public func >-> <T, U, V>(f: @escaping (T) -> U?, g: @escaping (U) -> V?) -> (T) -> V? {
+  return { x in f(x) >>- g }
+}
+
+infix operator >>- : FunctionCompositionPrecedence
+/**
+  flatMap a function over an optional value (left associative)
+  - If the value is `.none`, the function will not be evaluated and this will
+    return `.none`
+  - If the value is `.some`, the function will be applied to the unwrapped
+    value
+  - parameter f: A transformation function from type `T` to type `Optional<U>`
+  - parameter a: A value of type `Optional<T>`
+  - returns: A value of type `Optional<U>`
+*/
+public func >>- <T, U>(a: T?, f: (T) -> U?) -> U? {
+  return a.flatMap(f)
+}
